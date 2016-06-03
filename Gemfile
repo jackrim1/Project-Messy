7f804716757cd7fa5f3559b85e1081909520029e
Jack Riminton <jriminton@gmail.com>
2016-06-03 17:07:43 +0100

    facebook log in

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 gem 'devise'
+gem 'omniauth-facebook'
 
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :barracks
+
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
 
   root "barracks#index"
 
