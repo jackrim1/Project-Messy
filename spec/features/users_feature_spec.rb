556afbdc86c2992b43a2cee29e0f18515370af87
Jack Riminton <jriminton@gmail.com>
2016-06-04 11:04:09 +0100

    changed heroku log in details

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -34,5 +34,11 @@ feature "User can sign in and out" do
       expect(page).not_to have_link('Sign in')
       expect(page).not_to have_link('Sign up')
     end
+
+    it "should see an account page" do
+      visit('/')
+      expect(page).to have_link('My Account')
+    end
+
   end
 end
